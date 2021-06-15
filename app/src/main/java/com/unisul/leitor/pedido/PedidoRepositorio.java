3b420d871dafdc94175b89a9f3bf6d67ff8033af
gustavocnp95 <c98jjkdhgy>
2021-06-14 22:03:12 -0300

    implementa envio de pedido

--- app/src/main/java/com/unisul/leitor/pedido/PedidoRepositorio.java
+++ app/src/main/java/com/unisul/leitor/pedido/PedidoRepositorio.java
@@ -8,6 +8,8 @@ import androidx.annotation.NonNull;
 import com.unisul.leitor.BaseRepositorio;
 import com.unisul.leitor.common.AndroidUtils;
 import com.unisul.leitor.pedido.data.PedidoRest;
+import com.unisul.leitor.pedido.db.PedidoEntity;
+import com.unisul.leitor.pedido.model.PedidoLeitura;
 import com.unisul.leitor.pedido.model.PedidoListagem;
 
 import java.util.Collections;
@@ -19,11 +21,13 @@ import io.reactivex.rxjava3.core.Single;
 
 public class PedidoRepositorio extends BaseRepositorio {
     public Completable enviarPedido(@NonNull final Context context,
-                                    final long idPedido) {
+                                    @NonNull final PedidoLeitura pedidoLeitura) {
         if (!AndroidUtils.isNetworkAvailable(context)) {
             return Completable.error(new NetworkErrorException("Sem conexão a internet!"));
         }
-        return Completable.complete();
+        return getRestService(PedidoRest.class)
+                .sendItensPedido(pedidoLeitura)
+                .onErrorResumeNext(Completable::error);
     }
 
     public Single<List<PedidoListagem>> syncPedidosPendentes(@NonNull final Context context) {
