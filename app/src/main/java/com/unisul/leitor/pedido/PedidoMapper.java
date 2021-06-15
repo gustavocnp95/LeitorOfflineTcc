2524dbd4e5497045d638ba87d06a6c999ce0a760
gustavocnp95 <c98jjkdhgy>
2021-06-14 22:03:00 -0300

    cria mapper para novos dtos

--- app/src/main/java/com/unisul/leitor/pedido/PedidoMapper.java
+++ app/src/main/java/com/unisul/leitor/pedido/PedidoMapper.java
@@ -4,11 +4,17 @@ import androidx.annotation.NonNull;
 
 import com.unisul.leitor.novaleiturapedido.model.PedidoFiltro;
 import com.unisul.leitor.pedido.db.PedidoEntity;
+import com.unisul.leitor.pedido.itempedido.db.ItemPedidoEntity;
+import com.unisul.leitor.pedido.model.ItemPedidoLeitura;
+import com.unisul.leitor.pedido.model.PedidoLeitura;
 import com.unisul.leitor.pedido.model.PedidoListagem;
 import com.unisul.leitor.statuspedidos.model.StatusPedidoListagem;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
 
 public final class PedidoMapper {
 
@@ -59,4 +65,20 @@ public final class PedidoMapper {
                 pedidoEntity.getTipoItens(),
                 pedidoEntity.getNomeCliente());
     }
+
+    public static PedidoLeitura toPedidoLeitura(final long codPedido, @NonNull final List<ItemPedidoEntity> itensPedido) {
+        if (itensPedido.isEmpty()) {
+            throw new IllegalStateException("Lista nao pode ser vazia!");
+        }
+        return new PedidoLeitura(
+                codPedido,
+                itensPedido.stream()
+                        .map(PedidoMapper::toItemPedidoLeitura)
+                        .collect(Collectors.toList()));
+    }
+
+    public static ItemPedidoLeitura toItemPedidoLeitura(@NonNull final ItemPedidoEntity itemPedidoEntity) {
+        return new ItemPedidoLeitura(itemPedidoEntity.getCodigoBarras());
+    }
+
 }
